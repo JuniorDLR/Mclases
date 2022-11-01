de40839bd07e3b7a6eba5fc50b3bbcc2f5c7cbd5
JuniorDLR <86742947+JuniorDLR@users.noreply.github.com>
2022-10-31 23:42:54 -0600

    Update MainActivity.kt
    hola

--- app/src/main/java/ni/edu/uca/mclases/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/mclases/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     val num1 =1
     val num2=2
+    val num3=3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
