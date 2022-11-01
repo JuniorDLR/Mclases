5cadf4362b4d5b62095186378cd44ae2363a5648
JuniorDLR <juniordelarocha15@hotmail.com>
2022-10-31 23:48:12 -0600

    hola

--- app/src/main/java/ni/edu/uca/mclases/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/mclases/MainActivity.kt
@@ -4,15 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    val num1 =1
-    val num2=2
-    val num3=3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-val num1=1
-        val junior="Hola"
+
 
     }
 }
